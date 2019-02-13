006913bffd0456ee1ba9ae8a0867f44672f87d95
dapengchina <aoqishequ@163.com>
2019-02-13 18:27:43 +0800

    Change-Id: I6ac2421ea5b75e793686d0b057661f7eb4e9cfba

--- src/main/java/com/csi/sbs/gateway/Application.java
+++ src/main/java/com/csi/sbs/gateway/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
@@ -12,6 +13,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication
 @CrossOrigin // 解决跨域请求
 public class Application {
+	
+	@Bean
+	public AuthorizedZuulFilter sessionPreFilter() {
+		return new AuthorizedZuulFilter();
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
