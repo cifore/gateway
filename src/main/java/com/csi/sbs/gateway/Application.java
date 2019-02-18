a803ef9236cdd800394965c4d96f7cbe7a8bb1f9
dapengchina <aoqishequ@163.com>
2019-02-18 12:07:26 +0800

    Change-Id: I372cbfe34fee94def884066788d30c1266fcc8a2

--- src/main/java/com/csi/sbs/gateway/Application.java
+++ src/main/java/com/csi/sbs/gateway/Application.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-@CrossOrigin // 解决跨域请求
+@CrossOrigin// 解决跨域请求
 public class Application {
 	
 	
