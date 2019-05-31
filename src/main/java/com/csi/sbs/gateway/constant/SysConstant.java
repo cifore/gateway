3e45a56bbf158838ab2015aa700f5e26f39924ae
dapengchina <aoqishequ@163.com>
2019-05-31 10:45:30 +0800

    Change-Id: Ice134729642ad234f788db3cd27ca4a8803d7b85

--- src/main/java/com/csi/sbs/gateway/constant/SysConstant.java
+++ src/main/java/com/csi/sbs/gateway/constant/SysConstant.java
@@ -58,7 +58,7 @@ public class SysConstant {
 		   list.add("/deposit/deposit/rate/deleteAmountRange");
 		   list.add("/deposit/deposit/rate/getAmountRangeInfoById");
 		   list.add("/deposit/deposit/account/sandboxSearch");
-//		   list.add("/deposit/deposit/account/customerCreation");
+		   list.add("/deposit/deposit/account/customerCreation");
 		   
 		   list.add("/investment/investment/datehandle/sandboxSearch");
 		   
